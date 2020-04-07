4f6ef4bf5889c960f4320f9f7760747fad96ee9d
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-04-07 18:17:19 -0300

    Carregando dados da API 2

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return (
-    <h1>
-      Repository:
-      {decodeURIComponent(match.params.repository)}
-    </h1>
-  );
+export default class Repository extends Component {
+  constructor(props) {
+    super(props);
+
+    // State do componente
+    this.state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props; // Props do componente
+    const repoName = decodeURIComponent(match.params.repository); // Nome
+
+    // Faz duas chamadas de API ao mesmo tempo
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    // Set os dados dos state
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <h1>Repository:</h1>;
+  }
 }
