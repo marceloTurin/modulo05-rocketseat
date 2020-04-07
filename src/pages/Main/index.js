735ab3aed5c2ad37e06bd57f596daf835da63d9c
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-04-07 17:55:17 -0300

    Carregando dados da API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
 import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
-  state = {
-    newRepo: '',
-    repositories: [],
-    loading: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = { newRepo: '', repositories: [], loading: false };
+  }
 
   // Carregar os dados do localStorage
   componentDidMount() {
@@ -85,7 +85,9 @@ export default class Main extends Component {
           {repositories.map((repository) => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
